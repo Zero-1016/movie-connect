e242a875af1ee3119c6aa63d2ce44985160b90f7
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-04-29 03:15:56 +0900

    feat: 댓글 모아보기 구현

--- src/entities/ui/MovieImagesSlide.tsx
+++ src/entities/ui/MovieImagesSlide.tsx
@@ -1,12 +1,15 @@
 "use client"
 
+import { Avatar } from "@mui/material";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import Link from "next/link";
 
 import { IMAGE_SIZE, QUERY_KEY } from "@/shared/api/constants";
 import { getImages, getImageUrl } from "@/shared/api/lib";
+import { generateMockComments } from "@/shared/api/model/comment";
 import { useDragHandler } from "@/shared/hook/useDragHandler";
+import { timeAgo } from "@/shared/lib/util";
 
 import styles from './movie-images-slide.module.scss'
 
@@ -24,11 +27,13 @@ export function MovieImagesSlide({ movieId }: Props) {
     const { ref, onMouseDown } = useDragHandler()
 
     const backDropList = ImageList.backdrops.slice(0, 20)
+    const comments = generateMockComments(4)
 
     return (
-        <>
+        <section className={styles.container}>
+            <h3 className={styles.subTitle}>장면 미리보기</h3>
             {backDropList.length > 0 && <ul
-                className={styles.container}
+                className={styles.backDropList}
                 onMouseDown={onMouseDown}
                 ref={ref}
             >
@@ -38,6 +43,19 @@ export function MovieImagesSlide({ movieId }: Props) {
                     src={getImageUrl(backDrop.file_path, IMAGE_SIZE.backdrop.w300)}
                     alt={"배경 이미지 미리보기"} width={280} height={140}/></Link>)}
             </ul>}
-        </>
+            <h3 className={styles.subTitle}>리뷰 모아보기</h3>
+            {comments.length > 0 && <ul className={styles.commentContainer}>
+                {comments.map(comment => <li className={styles.comment} key={comment.id}>
+                    <div className={styles.commentHeader}>
+                        <div className={styles.commentAvatar}>
+                            <Avatar src={comment.img_url}
+                                    alt={comment.username}/> {comment.username}
+                        </div>
+                        <div>{timeAgo(comment.create_at)}</div>
+                    </div>
+                    <div>{comment.comment}</div>
+                </li>)}
+            </ul>}
+        </section>
     )
 }
--- src/shared/lib/util/index.ts
+++ src/shared/lib/util/index.ts
@@ -1,2 +1,3 @@
 export { getDecimal } from './get-decimal'
 export { getHour } from './get-hours'
+export { timeAgo } from './time-age'
