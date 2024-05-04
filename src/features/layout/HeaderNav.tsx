1d035e212b0346da36f75c828ea243518f7c51c5
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-05-04 22:56:52 +0900

    feat: 툴팁을 추가한다

--- src/features/layout/HeaderNav.tsx
+++ src/features/layout/HeaderNav.tsx
@@ -13,12 +13,12 @@ import styles from './header-nav.module.scss'
 
 export function HeaderNav() {
     return <nav className={styles.container}>
-        <NavButton activeSvg={<HomeIcon/>} notActiveSvg={<HomeOutlinedIcon/>} link="/home" title="Home"/>
-        <NavButton activeSvg={<FavoriteIcon/>} notActiveSvg={<FavoriteBorderOutlinedIcon/>} link="/list/popular"
+        <NavButton tooltip={"홈으로"} activeSvg={<HomeIcon/>} notActiveSvg={<HomeOutlinedIcon/>} link="/home" title="Home"/>
+        <NavButton tooltip={"인기영화 보러가기"} activeSvg={<FavoriteIcon/>} notActiveSvg={<FavoriteBorderOutlinedIcon/>} link="/list/popular"
                    title="Popular"/>
-        <NavButton activeSvg={<StarIcon/>} notActiveSvg={<StarBorderOutlinedIcon/>} link="/list/top-rated"
-                   title="TopRated"/>
-        <NavButton activeSvg={<EditCalendarIcon/>} notActiveSvg={<EditCalendarOutlinedIcon/>} link="/list/upcoming"
+        <NavButton tooltip={"별점이 높은 영화 보러가기"} activeSvg={<StarIcon/>} notActiveSvg={<StarBorderOutlinedIcon/>} link="/list/top-rated"
+                   title="Top Rated"/>
+        <NavButton tooltip={"최신 영화 보러가기"} activeSvg={<EditCalendarIcon/>} notActiveSvg={<EditCalendarOutlinedIcon/>} link="/list/upcoming"
                    title="Recent"/>
     </nav>
 }
--- src/features/layout/NavButton.tsx
+++ src/features/layout/NavButton.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Tooltip } from "@mui/material";
 import Button from "@mui/material/Button";
 import classNames from "classnames";
 import { usePathname, useRouter } from "next/navigation";
@@ -12,10 +13,11 @@ type Props = {
     notActiveSvg: ReactNode
     link: string,
     title: string,
+    tooltip?: string,
     activeAllow?: boolean,
 }
 
-export function NavButton({ title, notActiveSvg, activeSvg, link, activeAllow = false }: Props) {
+export function NavButton({ title, notActiveSvg, activeSvg, link, activeAllow = false, tooltip }: Props) {
     const router = useRouter()
     const path = usePathname();
     const isActive = path === link
@@ -26,6 +28,21 @@ export function NavButton({ title, notActiveSvg, activeSvg, link, activeAllow = 
 
     const styleActive = activeAllow && isActive && styles.active
 
-    return <Button className={classNames(styles.button, styleActive)} onClick={onClick}>{isActive ? activeSvg : notActiveSvg}
-        <span>{title}</span></Button>
+    if (tooltip) {
+        return <Tooltip title={tooltip}>
+            <Button className={classNames(styles.button, styleActive)}
+                    onClick={onClick}>{isActive ? activeSvg : notActiveSvg}
+                <span>{title}</span></Button>
+        </Tooltip>
+    }
+
+    return <Button className={classNames(styles.button, styleActive)}
+                   onClick={onClick}>
+        {tooltip ? <Tooltip title={tooltip}>
+            <>
+                {isActive ? activeSvg : notActiveSvg}
+                {title}
+            </>
+        </Tooltip> : <>{isActive ? activeSvg : notActiveSvg}
+            {title}</>}</Button>
 }
